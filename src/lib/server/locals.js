96387e6dab6105153992f18a221edfecc52d8dc4
Jason Creviston <jcreviston@pm.me>
2022-10-13 08:10:54 -0400

    remove dead code

--- src/lib/server/locals.js
+++ src/lib/server/locals.js
@@ -14,35 +14,5 @@ export const locals = async ({ event, resolve }) => {
     refresh_token: cookies['sb-refresh-token']
   }
 
-  // if (access_token) {
-  //   const jwt = decode(access_token)
-  //   const expires = jwt.exp - (Date.now() / 1000)
-
-  //   if (expires < 1) {
-  //     try {
-  //       const { data, error } = await supabaseClient.auth.setSession({access_token, refresh_token})
-  //       if (error) console.error(error)
-  //       if (data.session) {
-  //         const refreshCookies = Object.entries({
-  //           'sb-user': data.session.user,
-  //           'sb-access-token': data.session.access_token,
-  //           'sb-refresh-token': data.session.refresh_token
-  //         })
-
-  //         refreshCookies.forEach(([name, value]) => {
-  //           event.cookies.set(name, JSON.stringify(value), config.supakit.cookie.options)
-  //         })
-  //         event.locals.session = {
-  //           user: data.session?.user,
-  //           access_token: data.session?.access_token,
-  //           refresh_token: data.session?.refresh_token
-  //         }
-  //       } else { setLocals() }
-  //     } catch (err) {
-  //       console.error(err)
-  //     }
-  //   } else { setLocals() }
-  // } else { setLocals() }
-
   return await resolve(event)
 }
