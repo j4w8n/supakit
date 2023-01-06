98f48659ac7d659b36a4265c76f21d62f9ccd284
Jason Creviston <jason@malynium.com>
2023-01-06 07:48:25 -0500

    add js extensions

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -1,6 +1,6 @@
 import { sequence } from "@sveltejs/kit/hooks"
-import { cookies } from "./cookies"
-import { client } from "./client"
-import { locals } from "./locals"
+import { cookies } from "./cookies.js"
+import { client } from "./client.js"
+import { locals } from "./locals.js"
 
 export const auth = sequence(cookies, locals, client)
