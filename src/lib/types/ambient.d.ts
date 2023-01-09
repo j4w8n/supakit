56d8f2314c29fca7a83001d3ee363318b5aa55ff
Jason Creviston <jason@malynium.com>
2023-01-09 14:56:04 -0500

    add files

---
+++ src/lib/types/ambient.d.ts
@@ -0,0 +1,42 @@
+import type { SupakitConfig } from 'supakit/types'
+import type { Plugin } from 'vite'
+
+/**
+ * Supakit's configuration
+ */
+declare module '$supakit/config' {
+  /**
+   * A function which returns the merged result of the default configuration, 
+   * and any user-supplied configuration via `supakit.config.js` in the project's root directory.
+   */
+  export function getConfig(): SupakitConfig
+}
+
+/**
+ * Supakit's types
+ */
+declare module 'supakit/types' {
+
+}
+
+/**
+ * Supakit's Vite plugin
+ */
+declare module 'supakit/vite' {
+  export function supakit(): Plugin
+}
+
+/**
+ * Declare typings for SvelteKit's interfaces, within the library
+ */
+declare global {
+	namespace App {
+		interface Locals {
+			session: {
+				user: string | null
+				access_token: string | null
+				refresh_token: string | null
+			}
+		}
+	}
+}
---
+++ src/lib/types/index.d.ts
@@ -0,0 +1,22 @@
+import type { User, Session } from '@supabase/supabase-js'
+import type { Writable } from 'svelte/store'
+import type { CookieSerializeOptions } from 'cookie'
+
+export type SupakitConfig = {
+  [key: string]: any
+  supakit: {
+		sessionStore?: boolean;
+    cookie?: {
+      options?: CookieSerializeOptions;
+      route?: RequestInfo | URL;
+    }
+    redirects?: {
+      login?: RequestInfo | URL;
+      logout?: RequestInfo | URL;
+    }
+	}
+}
+
+export type StateChangeCallback = ({event, session}: {event: string, session: Session | null}) => void
+
+export type StateChange = (store?: Writable<User | null> | null, callback?: StateChangeCallback | null) => void
