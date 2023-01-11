8d9fb66e97b84b57c0a30ef43f07920af52f9e65
Jason Creviston <jason@malynium.com>
2023-01-11 12:30:25 -0500

    move index file

---
+++ src/lib/exports/index.js
@@ -0,0 +1,7 @@
+export { auth } from '../server/auth'
+export { locals } from '../server/locals'
+export { cookies } from '../server/cookies'
+export { supabaseClient } from '../client/client'
+export { client, supabaseServerClient } from '../server/client'
+export { state } from '../client/state'
+export { getSession } from '../client/store'
