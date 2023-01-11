f19921968cc82bb80c2cbed9071c327c10f50faf
Jason Creviston <jason@malynium.com>
2023-01-10 19:09:33 -0500

    type stuff

--- src/lib/types/index.d.ts
+++ src/lib/types/index.d.ts
@@ -1,6 +1,7 @@
 import type { User, Session } from '@supabase/supabase-js'
 import type { Writable } from 'svelte/store'
 import type { CookieSerializeOptions } from 'cookie'
+import './ambient.js'
 
 export type SupakitConfig = {
   [key: string]: any
