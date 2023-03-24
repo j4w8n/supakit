4f24b16dab328315ee8e8a8230fa805e524c9c44
Jason Creviston <jason@malynium.com>
2023-03-23 21:08:12 -0400

    tighten security for non-server client

--- src/lib/client/client.ts
+++ src/lib/client/client.ts
@@ -1,4 +1,11 @@
 import { createClient, SupabaseClient } from "@supabase/supabase-js"
 import { env } from '$env/dynamic/public'
 
-export const supabaseClient: SupabaseClient = createClient(env.PUBLIC_SUPABASE_URL || '', env.PUBLIC_SUPABASE_ANON_KEY || '')
+export const supabaseClient: SupabaseClient = createClient(
+  env.PUBLIC_SUPABASE_URL || '', env.PUBLIC_SUPABASE_ANON_KEY || '',
+  {
+    auth: {
+      persistSession: false
+    }
+  }
+)
