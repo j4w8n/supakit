a4ab58a569ff62706f8f89f98027d22ab0fb43bf
Jason Creviston <jason@malynium.com>
2023-03-30 08:06:53 -0400

    tatu

--- src/lib/server/locals.ts
+++ src/lib/server/locals.ts
@@ -4,23 +4,22 @@ import { env } from '$env/dynamic/public'
 import { decodeBase64URL } from '../utils.js'
 
 export const locals = (async ({ event, resolve }) => {
-  const cookie: Session = event.cookies.get('sb-session') ? JSON.parse(event.cookies.get('sb-session') || '') : null
-  console.log('cookie', cookie)
+  const session: Session | null = event.cookies.get('sb-session') ? JSON.parse(event.cookies.get('sb-session') || '') : null
 
   // get jwt info
-  const token = cookie ? JSON.parse(decodeBase64URL(cookie.access_token.split('.')[1])) : null
+  const token = session ? JSON.parse(decodeBase64URL(session.access_token.split('.')[1])) : null
   
-  event.locals.session = token ? {
-    user: cookie.user,
-    access_token: cookie.access_token,
-    refresh_token: cookie.refresh_token,
+  event.locals.session = session ? {
+    user: session.user,
+    access_token: session.access_token,
+    refresh_token: session.refresh_token,
     expires_in: Math.floor(token.exp - (Date.now()/1000)),
     token_type: 'bearer'
   } : null
 
-  event.locals.supabase = event.locals.session ? createClient(env.PUBLIC_SUPABASE_URL || '', env.PUBLIC_SUPABASE_ANON_KEY || '', {
+  event.locals.supabase = session ? createClient(env.PUBLIC_SUPABASE_URL || '', env.PUBLIC_SUPABASE_ANON_KEY || '', {
     global: {
-      headers: { 'Authorization': `Bearer ${event.locals.session.access_token}` }
+      headers: { 'Authorization': `Bearer ${session.access_token}` }
     },
     auth: {
       persistSession: false,
@@ -29,10 +28,10 @@ export const locals = (async ({ event, resolve }) => {
     }
   }) : null
 
-  if (event.locals.supabase && event.locals.session) {
-    await event.locals.supabase.auth.setSession({ 
-      access_token: event.locals.session.access_token, 
-      refresh_token: event.locals.session.refresh_token 
+  if (session) {
+    await event.locals.supabase!.auth.setSession({ 
+      access_token: session.access_token, 
+      refresh_token: session.refresh_token 
     })
   }
 
