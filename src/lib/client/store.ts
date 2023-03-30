e874063f19daa8cc34e7a32981e055861961c7df
Jason Creviston <jason@malynium.com>
2023-03-30 07:38:32 -0400

    update Writable type to Session

--- src/lib/client/store.ts
+++ src/lib/client/store.ts
@@ -1,15 +1,15 @@
 import { setContext, getContext, hasContext } from 'svelte'
 import { writable, type Writable } from 'svelte/store'
-import type { User } from '@supabase/supabase-js'
+import type { Session } from '@supabase/supabase-js'
 
 const keys = { session: Symbol() }
 
-const initSession = (): Writable<User | null> => {
-  setContext(keys.session, writable() as Writable<User | null>)
+const initSession = (): Writable<Session | null> => {
+  setContext(keys.session, writable() as Writable<Session | null>)
 
   return getContext(keys.session)
 }
 
-export const getSession = (): Writable<User | null> => {
+export const getSession = (): Writable<Session | null> => {
   return hasContext(keys.session) ? getContext(keys.session) : initSession()
 }
