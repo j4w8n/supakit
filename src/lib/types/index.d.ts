98bc3bc9ed10aa047579155406d2329ab7dccdb5
Jason Creviston <jason@malynium.com>
2023-03-30 16:51:27 -0400

    tatu

--- src/lib/types/index.d.ts
+++ src/lib/types/index.d.ts
@@ -1,4 +1,4 @@
-import type { Session, SupabaseClient } from '@supabase/supabase-js'
+import type { AuthChangeEvent, Session, SupabaseClient } from '@supabase/supabase-js'
 import type { Writable } from 'svelte/store'
 import type { CookieSerializeOptions } from 'cookie'
 import type { Handle } from '@sveltejs/kit'
@@ -7,13 +7,13 @@ export type CookieOptions = {
   [key: string]: any
 }
 
-export type StateChangeCallback = ({event, session}: {event: string, session: Session | null}) => void
+export type StateChangeCallback = ({ event, session }: { event: AuthChangeEvent, session: Session | null }) => void
 
 export function supakitAuth(): Handle
 export function supabaseAuthStateChange(
   client?: SupabaseClient | null,
   store?: Writable<Session | null> | null, 
-  callback?: (({event, session}: {event: string, session: Session | null}) => void) | null
+  callback?: ({ event, session }: { event: AuthChangeEvent, session: Session | null }) => void | null
 ): void
 export function getSession(): Writable<Session | null>
 export function setCookieOptions({}: CookieSerializeOptions): void
