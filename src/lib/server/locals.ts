88b3712e4928770b329c5a70caf8cb0dfd29f934
Jason Creviston <jason@malynium.com>
2023-04-06 07:31:37 -0400

    destructure event

--- src/lib/server/locals.ts
+++ src/lib/server/locals.ts
@@ -5,14 +5,15 @@ import { decodeBase64URL } from '../utils.js'
 import { getCookieOptions } from '../config/index.js'
 
 export const locals = (async ({ event, resolve }) => {
-  const session: Session | null = event.cookies.get('sb-session') ? JSON.parse(event.cookies.get('sb-session') || '') : null
-  const provider_token: string = event.cookies.get('sb-provider-token') ? JSON.parse(event.cookies.get('sb-provider-token') || '') : null
-  const provider_refresh_token: string = event.cookies.get('sb-provider-refresh-token') ? JSON.parse(event.cookies.get('sb-provider-refresh-token') || '') : null
+  const { cookies, locals } = event
+  const session: Session | null = cookies.get('sb-session') ? JSON.parse(cookies.get('sb-session') || '') : null
+  const provider_token: string = cookies.get('sb-provider-token') ? JSON.parse(cookies.get('sb-provider-token') || '') : null
+  const provider_refresh_token: string = cookies.get('sb-provider-refresh-token') ? JSON.parse(cookies.get('sb-provider-refresh-token') || '') : null
 
   // get jwt info
   const token = session ? JSON.parse(decodeBase64URL(session.access_token.split('.')[1])) : null
   
-  event.locals.session = session ? {
+  locals.session = session ? {
     provider_token,
     provider_refresh_token,
     access_token: session.access_token,
@@ -23,7 +24,7 @@ export const locals = (async ({ event, resolve }) => {
     user: session.user
   } : null
 
-  event.locals.supabase = createClient(env.PUBLIC_SUPABASE_URL || '', env.PUBLIC_SUPABASE_ANON_KEY || '', {
+  locals.supabase = createClient(env.PUBLIC_SUPABASE_URL || '', env.PUBLIC_SUPABASE_ANON_KEY || '', {
     auth: {
       persistSession: false,
       autoRefreshToken: false,
@@ -32,13 +33,13 @@ export const locals = (async ({ event, resolve }) => {
   })
 
   if (session) {
-    await event.locals.supabase.auth.setSession({ 
+    await locals.supabase.auth.setSession({ 
       access_token: session.access_token, 
       refresh_token: session.refresh_token 
     })
   }
 
-  event.locals.cookie_options = getCookieOptions()
+  locals.cookie_options = getCookieOptions()
 
   return await resolve(event)
 }) satisfies Handle
