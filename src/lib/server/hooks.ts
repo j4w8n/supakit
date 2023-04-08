51d8073d46969e09eed22ffa315614e45e742be8
Jason Creviston <jason@malynium.com>
2023-04-08 10:22:42 -0400

    fix: cannot use sequence() with hook functions

--- src/lib/server/hooks.ts
+++ src/lib/server/hooks.ts
@@ -1,6 +1,7 @@
 import { sequence } from "@sveltejs/kit/hooks"
 import { cookies } from "./cookies.js"
 import { locals } from "./locals.js"
+import type { Handle } from "@sveltejs/kit"
 
-export const supakit = sequence(cookies, locals)
-export const supakitLite = cookies
+export const supakit: Handle = sequence(cookies, locals)
+export const supakitLite: Handle = cookies
