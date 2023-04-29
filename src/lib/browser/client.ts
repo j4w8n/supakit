83f6cd6e59a44c194c234d33d85834b727b6ecd6
Jason Creviston <jason@malynium.com>
2023-04-29 11:46:34 -0400

    bump version

--- src/lib/browser/client.ts
+++ src/lib/browser/client.ts
@@ -14,7 +14,7 @@ export const createBrowserClient = <
       ...options?.global,
       headers: {
         ...options?.global?.headers,
-        'X-Client-Info': 'supakit@v1.0.0-next.112'
+        'X-Client-Info': 'supakit@v1.0.0-next.113'
       }
     },
     auth: {
