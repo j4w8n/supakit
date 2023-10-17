8bea62cdfefffa7527e1965a79d6caa0c018abe9
Jason Creviston <jason@malynium.com>
2023-10-17 16:47:02 -0400

    fix: force sameSite to be lax for config cookie

--- src/lib/config/index.ts
+++ src/lib/config/index.ts
@@ -60,7 +60,8 @@ export const supabaseConfig = (cookies?: Cookies) => {
       cookies.set('sb-config', JSON.stringify(merged_config), {
         ...cookie_options,
         httpOnly: false,
-        maxAge: COOKIE_DEFAULTS.maxAge
+        maxAge: COOKIE_DEFAULTS.maxAge,
+        sameSite: 'lax'
       })
     }
   }
